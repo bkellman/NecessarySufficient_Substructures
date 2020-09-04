daf6a1d3cf66d0f62e14920317ac89b07e39c856
Ben Kellman <bkellman@ucsd.edu>
2020-09-03 20:53:20 -0700

    vectorize compute

--- scripts/CPM.py
+++ scripts/CPM.py
@@ -6,6 +6,10 @@ Conditional Probability Matrix (CPM)
 import pandas as pd
 import itertools
 
+def substr_prop(sub,mx,b):
+    return (((b[sub]*b[mx])>0).sum())/(b[sub]>0).sum()
+
+
 def conditional_vect(glycan_id, substructure_info):
     """
     Generates the substructure conditional probability matrix for 
@@ -13,16 +17,47 @@ def conditional_vect(glycan_id, substructure_info):
     substructure information. Returns a conditional probability matrix
     where columns are conditions; P_ij is P(i|j). *appearance is binary*
     """
+    
     substructure_db = pd.DataFrame(data=substructure_info)
     substructures = substructure_db[substructure_db[glycan_id] != 0].index
     
     b = substructure_db.T
     mx = substructures[-1]
-    cond = pd.DataFrame( {glycan_id : [((b[sub]*b[mx])>0).sum()/(b[sub]>0).sum() for sub in substructures]} ,
+    
+    cond = pd.DataFrame( {glycan_id : [substr_prop(sub,mx,b) for sub in substructures]} ,
                         index = substructures)
     return cond
     
     
+def substr_cbx_prop(sub,mx,b):
+    return (((b[sub[0]]*b[sub[1]]*b[mx])>0).sum())/(b[sub]>0).sum()
+
+
+def conditional_cbx_vect(glycan_id, substructure_info):
+    """
+    Generates the substructure conditional probability matrix for 
+    an input glycan. Input glycans must be an entry in the original 
+    substructure information. Returns a conditional probability matrix
+    where columns are conditions; P_ij is P(i|j). *appearance is binary*
+    """
+    
+    substructure_db = pd.DataFrame(data=substructure_info)
+    substructures = substructure_db[substructure_db[glycan_id] != 0].index
+    substructures = list(itertools.combinations(substructures,2))
+    
+    b = substructure_db.T
+    mx = substructures[-1]
+    
+    cond = pd.DataFrame( {glycan_id : [substr_cbx_prop(sub,mx,b) for sub in substructures]} ,
+                        index = substructures)
+    return cond
+    
+  
+
+
+
+
+########################
 
 def conditional_matrix(glycan_id, substructure_info,largest_only=True):
     """
